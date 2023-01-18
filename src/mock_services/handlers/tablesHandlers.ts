a08321377be2fb0974ef6661aa2dd3930e15e209
Jsusvalle <jesusvalle2001@hotmail.com>
2023-01-18 13:10:52 -0500

    fix: dependency msw

--- src/mock_services/handlers/tablesHandlers.ts
+++ src/mock_services/handlers/tablesHandlers.ts
@@ -6,28 +6,28 @@ import {
 } from 'mock_services';
 
 export const getMyTablesHandler = rest.get(
-  'http://exampleapi.com.co/my-tables',
+  'https://exampleapi.com.co/my-tables',
   (_, res, ctx) => {
     return res(ctx.delay(500), ctx.json(MY_TABLES_RESPONSE_DATA));
   }
 );
 
 export const getMyTablesErrorHandler = rest.get(
-  'http://exampleapi.com.co/my-tables',
+  'https://exampleapi.com.co/my-tables',
   (_, res, ctx) => {
     return res(ctx.delay(500), ctx.status(401));
   }
 );
 
 export const getAvailableTablesHandler = rest.get(
-  'http://exampleapi.com.co/available-tables',
+  'https://exampleapi.com.co/available-tables',
   (_, res, ctx) => {
     return res(ctx.delay(500), ctx.json(AVAILABLE_TABLES_RESPONSE_DATA));
   }
 );
 
 export const getAvailableTablesErrorHandler = rest.get(
-  'http://exampleapi.com.co/available-tables',
+  'https://exampleapi.com.co/available-tables',
   (_, res, ctx) => {
     return res(ctx.delay(500), ctx.status(401));
   }
